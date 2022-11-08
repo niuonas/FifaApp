7e2698fe9086a53db7a1bda06fd902c106256e5c
Nicu Iuonas <nicu2595@gmail.com>
2022-11-08 18:07:00 +0200

    Added edit and delete functionalities for players

---
+++ Migrations/20221104201742_ChangeOverallFromStringToInt.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebTest.Migrations
+{
+    public partial class ChangeOverallFromStringToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Overall",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Overall",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- ViewModels/Players/PlayerVM.cs
+++ ViewModels/Players/PlayerVM.cs
@@ -3,9 +3,10 @@ namespace WebTest.ViewModels.Players
 {
     public class PlayerVM
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Nationality { get; set; }
-        public string Overall { get; set; }
+        public int Overall { get; set; }
     }
 }
