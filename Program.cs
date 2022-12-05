6595519b032463f1c6d5768e7fe54c8ecd29befb
Nicu Iuonas <nicu2595@gmail.com>
2022-12-05 18:11:16 +0200

    Fixed cors for post methods

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
     {
-        policy.WithOrigins(@"http://localhost:4200");
+        policy.WithOrigins(@"http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
